25a588c192297433e1f6a94bbe958dbdc2ccd3e1
Daniel <danielmrezende@yahoo.com.br>
2026-01-23 10:06:11 -0300

    FEAT: movimentação da câmera e ajustes do player

--- src/modules/player/scripts/player.gd
+++ src/modules/player/scripts/player.gd
@@ -1,17 +1,25 @@
 extends VehicleBody3D
 
 @export var engine_power := 1200.0
-@export var brake_power := 40.0
+@export var brake_power := 9.0
 @export var max_steer := 0.5
 
 func _physics_process(delta):
-	var accelerate = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
-	var steer = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
-
-	engine_force = accelerate * engine_power
+	# Direção
+	var steer = Input.get_action_strength("move_left") - Input.get_action_strength("move_right")
 	steering = steer * max_steer
 
 	if Input.is_action_pressed("ui_select"):
 		brake = brake_power
+		engine_force = 0.0
+	
+	if Input.is_action_pressed("move_up"):
+		if not Input.is_action_pressed("ui_select"):
+			brake = 0.0
+			engine_force = engine_power
+	elif Input.is_action_pressed("move_down"):
+		if not Input.is_action_pressed("ui_select"):
+			brake = 0.0
+			engine_force = -engine_power * 0.6
 	else:
-		brake = 0.0
+		engine_force = 0.0
