a130f7dafd7721ade4977ce747204825464e3e06
Mario Cesario <mario.henrique.cesario@gmail.com>
2026-01-28 19:36:00 -0300

    feat: adiciona musica de fundo podendo desligar ou ligar com tecla m e sons dos clicks dos menus

--- src/main.gd
+++ src/main.gd
@@ -1,1 +1,25 @@
-extends Node3D
+extends Node2D
+
+@onready var _music_player: AudioStreamPlayer = $MusicPlayer
+var _music_on: bool = true
+
+func _ready() -> void:
+	# garante que comece com música ligada
+	if _music_player and _music_player.stream:
+		if not _music_player.playing:
+			_music_player.play()
+
+func _input(event: InputEvent) -> void:
+	# alterna música via ação `toggle_music`
+	if event.is_action_pressed("toggle_music"):
+		_toggle_music()
+
+func _toggle_music() -> void:
+	if not _music_player:
+		return
+	if _music_player.playing:
+		_music_player.stop()
+		_music_on = false
+	else:
+		_music_player.play()
+		_music_on = true
